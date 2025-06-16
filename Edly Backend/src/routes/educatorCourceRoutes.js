e83217f1a335f7f3daa8c9a5d33017e7fe269c09
Anmol Khatri <100636635+anmol4khatri@users.noreply.github.com>
2025-06-17 01:05:32 +0530

    Refactor course management by moving courseController logic to educatorCourseController; update routes accordingly.

--- Edly Backend/src/routes/educatorCourceRoutes.js
+++ Edly Backend/src/routes/educatorCourceRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const requireEducatorAuth = require("../middlewares/requireEducatorAuth");
-const { createCourse, getAllCourses, deleteCourse, updateCourse } = require("../controllers/courseController");
+const { createCourse, getAllCourses, deleteCourse, updateCourse } = require("../controllers/educatorCourseController");
 
 router.post("/educator/create_course", requireEducatorAuth, createCourse);
 router.get("/educator/get_all_courses", requireEducatorAuth, getAllCourses);
