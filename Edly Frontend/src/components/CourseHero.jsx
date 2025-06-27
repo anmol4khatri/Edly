ca2cb3a8284640685a44ea371341b5d83d04d05c
Anmol Khatri <100636635+anmol4khatri@users.noreply.github.com>
2025-06-28 01:32:39 +0530

    Add RatingDisplay component and integrate it into CourseHero; replace CardFooter content with rating display

--- Edly Frontend/src/components/CourseHero.jsx
+++ Edly Frontend/src/components/CourseHero.jsx
@@ -1,4 +1,5 @@
-import { Card, CardContent, CardHeader, CardDescription, CardTitle, CardFooter } from '@/components/ui/card'
+import { Card, CardHeader, CardDescription, CardTitle, CardFooter } from '@/components/ui/card'
+import RatingDisplay from './RatingDisplay'
 
 const CourseHero = () => {
     return (
@@ -8,11 +9,8 @@ const CourseHero = () => {
                     <CardTitle class="text-4xl w-6/10 font-extrabold pb-5">Docker and Kubernetes for beginners | DevOps journey</CardTitle>
                     <CardDescription class="text-xl w-6/10">Docker & Kubernetes Masterclass: Build, Deploy, & Scale on AWS, Azure, & GCP.</CardDescription>
                 </CardHeader>
-                {/* <CardContent>
-                    <p>Card Content</p>
-                </CardContent>*/}
                 <CardFooter>
-                    <p>Card Footer</p>
+                    <RatingDisplay rating={4.8} />
                 </CardFooter> 
             </Card>
         </div>
