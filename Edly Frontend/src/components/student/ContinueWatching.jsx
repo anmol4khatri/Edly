30702074e0a1e586c3044cff69570b7ae629d61c
Anmol Khatri <100636635+anmol4khatri@users.noreply.github.com>
2025-07-06 03:27:29 +0530

    Replace placeholder image with video in ContinueWatching component for enhanced content display.

--- Edly Frontend/src/components/student/ContinueWatching.jsx
+++ Edly Frontend/src/components/student/ContinueWatching.jsx
@@ -4,7 +4,7 @@ const ContinueWatching = () => {
     return(
         <div className="min-h-screen dark bg-background px-16 py-16 flex gap-6">
             <div className="w-6/10">
-                <img src="https://placehold.co/1920x1080" alt="video" className="aspect-video"/>
+                <video src="https://videos.pexels.com/video-files/7821663/7821663-hd_1920_1080_30fps.mp4" controls className="rounded"></video>
                 <p className="text-3xl font-bold mt-4">Docker and Kubernates</p>
             </div>
             <div className="w-4/10">
