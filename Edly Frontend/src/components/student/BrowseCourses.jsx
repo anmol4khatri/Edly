9a3c35e39a7891c1c30d1139b90a76d4ffbbac4f
Anmol Khatri <anmol@gravityer.com>
2025-09-15 23:32:48 +0530

    BrowseCourse Page Fix

--- Edly Frontend/src/components/student/BrowseCourses.jsx
+++ Edly Frontend/src/components/student/BrowseCourses.jsx
@@ -2,7 +2,7 @@ import CourseCard from "../CourseCard";
 
 const BrowseCourses = () => {
 	return (
-		<div className="py-10 max-sm:pt-2">
+		<div className="py-10 max-sm:pt-2 2xl:px-50">
 			<h1 className="text-3xl font-bold text-center">Browse Courses</h1>
 			<div className="flex flex-wrap gap-8 px-20 pt-8 max-sm:px-4 max-sm:pt-3">
 				<CourseCard price={5999} />
